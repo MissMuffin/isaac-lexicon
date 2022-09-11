d0505d374d85d38698d99930360a0cea300fe23c
Miss Muffin <fyyree@gmail.com>
2022-09-11 14:17:38 +0200

    added bottom navbar w/ search & lorem onclick modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {
+  Box,
+  Chip,
+  Container,
+  Divider,
+  Paper
+} from '@mui/material';
+import * as React from 'react';
+import ViewItemModal from './Modal';
+import Navbar from './Navbar';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const openModal = () => {
+    setOpenViewItemModal(true);
+  };
+
+  const closeModal = () => {
+    setOpenViewItemModal(false);
+  };
+
+	const [ openViewItemModal, setOpenViewItemModal ] = React.useState(false);
+	// const []
+	const [ value, setValue ] = React.useState(0);
+
+	const lorem = (
+		<p onClick={openModal}>
+			{`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id dignissim justo.
+   Nulla ut facilisis ligula. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+   Sed malesuada lobortis pretium.`}
+		</p>
+	);
+
+	return (
+		<Box>
+			<Container maxWidth="sm">
+				<ViewItemModal isOpen={openViewItemModal} onClose={closeModal} itemText={'Hello world'} />
+
+				{lorem}
+
+				<Divider>TEST</Divider>
+
+				{lorem}
+
+				<Divider>
+					<Chip label="BJHK" />
+				</Divider>
+			</Container>
+
+			<Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+				<Navbar />
+			</Paper>
+		</Box>
+	);
 }
 
 export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+
+const Search = styled('div')(({ theme }) => ({
+	position: 'relative',
+	borderRadius: theme.shape.borderRadius,
+	backgroundColor: alpha(theme.palette.common.white, 0.15),
+	'&:hover': {
+		backgroundColor: alpha(theme.palette.common.white, 0.25)
+	},
+	marginRight: theme.spacing(2),
+	marginLeft: 0,
+	width: '100%'
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+	padding: theme.spacing(0, 2),
+	height: '100%',
+	position: 'absolute',
+	pointerEvents: 'none',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center'
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+	color: 'inherit',
+	'& .MuiInputBase-input': {
+		padding: theme.spacing(1, 1, 1, 0),
+		// vertical padding + font size from searchIcon
+		paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+		width: '100%'
+	},
+    width: '100%'
+}));
+
+export default function PrimarySearchAppBar() {
+    
+	const [searchValue, setSearchValue] = React.useState('');
+
+    const handleChange = ({ target: { name, value } }) => {
+       console.log(value);
+	   setSearchValue(searchValue);
+     }
+
+	return (
+		// <Box sx={{ flexGrow: 1 }}>
+			<AppBar position="static">
+				<Toolbar>
+					<Search>
+						<SearchIconWrapper>
+							<SearchIcon />
+						</SearchIconWrapper>
+						<StyledInputBase placeholder="Search…" inputProps={{ 'aria-label': 'search' }} onChange={handleChange} />
+					</Search>
+				</Toolbar>
+			</AppBar>
+		// </Box>
+	);
+}
